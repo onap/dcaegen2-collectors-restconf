0a09b1751fc611900863eed0e47b13049156b481
s00370346 <swarup.nayak1@huawei.com>
2019-03-14 15:06:40 +0530

    Issue-ID: DCAEGEN2-1055 Generic RestConfCollector
    Change-Id: I1800affa2b34cbb7487c0d8411e078adec5a0c48
    Signed-off-by: s00370346 <swarup.nayak1@huawei.com>

---
+++ src/test/java/org/onap/dcae/TLSTest.java
@@ -0,0 +1,92 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * org.onap.dcaegen2.restconfcollector
+ * ================================================================================
+ * Copyright (C) 2019 Huawei Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcae;
+
+import io.vavr.collection.HashMap;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import static org.onap.dcae.TLSTest.HttpsConfiguration.USERNAME;
+import static org.onap.dcae.TLSTest.HttpsConfiguration.PASSWORD;
+
+public class TLSTest extends TLSTestBase {
+
+    @Nested
+    @Import(HttpConfiguration.class)
+    class HttpTest extends TestClassBase {
+
+        @Test
+        public void shouldHttpRequestSucceed() {
+            assertEquals(HttpStatus.OK, makeHttpRequest().getStatusCode());
+        }
+
+        @Test
+        public void shouldHttpsRequestFail() {
+            assertThrows(Exception.class, this::makeHttpsRequest);
+        }
+    }
+
+
+    static class HttpConfiguration extends TLSTestBase.ConfigurationBase {
+        @Override
+        protected void configureSettings(ApplicationSettings settings) {
+        }
+    }
+
+    static class HttpsConfiguration extends TLSTestBase.ConfigurationBase {
+        public static final String USERNAME = "TestUser";
+        public static final String PASSWORD = "TestPassword";
+
+        @Override
+        protected void configureSettings(ApplicationSettings settings) {
+            when(settings.keystoreFileLocation()).thenReturn(KEYSTORE.toString());
+            when(settings.keystorePasswordFileLocation()).thenReturn(KEYSTORE_PASSWORD_FILE.toString());
+            when(settings.rcc_keystoreFileLocation()).thenReturn(RCC_KEYSTORE.toString());
+            when(settings.rcc_keystorePasswordFileLocation()).thenReturn(RCC_KEYSTORE_PASSWORD_FILE.toString());
+            when(settings.authorizationEnabled()).thenReturn(true);
+            when(settings.validAuthorizationCredentials()).thenReturn(HashMap.of(USERNAME, "$2a$10$51tDgG2VNLde5E173Ay/YO.Fq.aD.LR2Rp8pY3QAKriOSPswvGviy"));
+        }
+    }
+
+    static class HttpsConfigurationWithTLSAuthentication extends HttpsConfiguration {
+        @Override
+        protected void configureSettings(ApplicationSettings settings) {
+            super.configureSettings(settings);
+            when(settings.authorizationEnabled()).thenReturn(false);
+            when(settings.clientTlsAuthenticationEnabled()).thenReturn(true);
+            when(settings.truststoreFileLocation()).thenReturn(TRUSTSTORE.toString());
+            when(settings.truststorePasswordFileLocation()).thenReturn(TRUSTSTORE_PASSWORD_FILE.toString());
+        }
+    }
+
+    static class HttpsConfigurationWithTLSAuthenticationAndBasicAuth extends HttpsConfigurationWithTLSAuthentication {
+        @Override
+        protected void configureSettings(ApplicationSettings settings) {
+            super.configureSettings(settings);
+            when(settings.authorizationEnabled()).thenReturn(true);
+        }
+    }
+}
